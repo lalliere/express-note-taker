559afa1f748e64b349618f6db9912ece10bef343
lalliere <emilylallier@yahoo.com>
2020-03-10 21:12:55 -0400

    commented out fs code in server.js

--- server.js
+++ server.js
@@ -15,27 +15,27 @@ app.use(express.json());
 require("./Develop/routes/apiRoutes")(app);
 require("./Develop/routes/htmlRoutes")(app);
 
-const readFileAsync = util.promisify(fs.readFile);
-const writeFileAsync = util.promisify(fs.writeFile);
+// const readFileAsync = util.promisify(fs.readFile);
+// const writeFileAsync = util.promisify(fs.writeFile);
 
-async function fileOfNotes() {
-  try {
-    let indexJs = await readFileAsync("index.js", "utf8");
-    let savedNotes = await readFileAsync("db.json", "utf8");
+// async function fileOfNotes() {
+//   try {
+//     let indexJs = await readFileAsync("index.js", "utf8");
+//     let savedNotes = await readFileAsync("db.json", "utf8");
 
-    let notesFile = [savedNotes].map(JSON.parse);
+//     let notesFile = [savedNotes].map(JSON.parse);
 
-    await writeFileAsync(
-      "db.json",
-      JSON.stringify(notesFile, null, 2),
-      "utf8"
-    );
+//     await writeFileAsync(
+//       "db.json",
+//       JSON.stringify(notesFile, null, 2),
+//       "utf8"
+//     );
 
-    console.log("Successfully wrote to 'db.json' file");
-  } catch (err) {
-    console.log(err);
-  }
-}
+//     console.log("Successfully wrote to 'db.json' file");
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
